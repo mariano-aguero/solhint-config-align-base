12ae12cb4b9186ed934f3745f84e32289d513dfa
Mariano Aguero <mariano.aguero@altoros.com>
2018-12-13 10:53:51 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# solhint-config-align-base
+
+[![npm version](https://badge.fury.io/js/solhint-config-base.svg)](http://badge.fury.io/js/solhint-config-base)
+
+This package provides Solhint's align base JS .solhintrc as an extensible shared config.
+
+## Usage
+
+We export align rules Solhint configurations for your usage.
+
+### solhint-config-align-base
+
+Our default export contains all align rules of our solhint.
+
+
+#### Installation
+- Execute command
+  ```sh
+  npm install --save-dev solhint-config-align-base
+  ```
+
+- Add `"extends": "align-base"` to your .solhintrc.
---
+++ rules/align.js
@@ -0,0 +1,27 @@
+module.exports = {
+  rules: {
+    // Array declaration must not contains spaces.
+    'array-declaration-spaces': 'error',
+
+    // Open bracket must be on same line. It must be indented by other constructions by space.
+    'bracket-align': 'error',
+
+    // Expression indentation is incorrect.
+    'expression-indent': 'error',
+
+    // Indentation is incorrect.
+    indent: ['error', 4],
+
+    // Mixed tabs and spaces.
+    'no-mix-tabs-and-spaces': 'error',
+
+    // Semicolon must not have spaces before.
+    'no-spaces-before-semicolon': 'error',
+
+    // Comma must be separated from next element by space.
+    'space-after-comma': 'error',
+
+    // Statement indentation is incorrect.
+    'statement-indent': 'error'
+  }
+}
